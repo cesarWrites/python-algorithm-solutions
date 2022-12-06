ca9a67b896a1e4f794982b5d8fd8d3aa5b61d7ba
Sarah Mukuti <cesarwrites24@gmail.com>
2022-12-06 21:42:04 +0300

    make changes

--- algorith-practice/algr1.py
+++ algorith-practice/algr1.py
@@ -65,3 +65,60 @@ print(remove_character("Python", 4))
 
 """Write a python program to exchange the 
 first and last characters in a given string and return the new string"""
+def exchange_string_characters(str1):
+    s = str1[-1:]
+    return s
+print(exchange_string_characters("abcd"))
+print(exchange_string_characters("a"))
+print(exchange_string_characters("xy"))
+
+"""Write a python program to remove the character in a given position of a given string. The given position will be in the range 0..string length -1 inclusive. """
+def remove_character_string(str1,pos):
+    s = str1.replace(str1[pos],'')
+    return s
+print(remove_character_string("python",1))
+print(remove_character_string("Python",0))
+print(remove_character_string("Python", 4))
+
+"""Write a python program to exchange the first and last characters in a given string and return the new string."""
+def swap_characters(str1):
+    first_char = str1[0]
+    last_char = str1[-1]
+    s = last_char + str1[1:-1] + first_char
+    return s
+print(swap_characters("python"))
+print(swap_characters("Java"))
+print(swap_characters("Ruby"))
+
+"""Write a python program to create a new string which is 4 copies of the 2 front characters of a given string. If the given string length is less than 2 return the original string"""
+def create_new_string(str1):
+    if(len(str1) >=2 ):
+        s = str1[0:2]
+        return s + s + s + s
+    return str1
+print(create_new_string("C Sharp"))
+print(create_new_string("JS"))
+print(create_new_string("a"))
+
+""" Write a python program to create a new string with the last char added at the front and back of a given string of length 1 or more"""
+def create_new_string(str1):
+    last_char = str1[-1]
+    if (len(str1) > 1):
+        str1 = last_char + str1 + last_char
+        return str1
+print(create_new_string("Red"))
+print(create_new_string("Green"))
+print(create_new_string("Blue"))
+print(create_new_string("a"))
+
+"""Write a python program to check if a given positive number is a multiple of 3 or a multiple of 7"""
+#Check if number is positive
+def multiple_of_three_or_seven(n):
+    if(n >= 0):
+        if ((n % 3 == 0) or (n % 7 == 0)):
+            return True
+        return False
+print(multiple_of_three_or_seven(3))
+print(multiple_of_three_or_seven(7))
+print(multiple_of_three_or_seven(14))
+print(multiple_of_three_or_seven(20))
